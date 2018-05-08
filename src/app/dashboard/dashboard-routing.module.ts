d3cd768d58722135ac9d5ccd761f334b57cedb8b
Peter Horvath <peter.horvath@finnoconsult.at>
2018-05-08 15:40:42 +0200

    add blank components to manage video

---
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { VideoDashboardComponent } from './video-dashboard/video-dashboard.component';
+import { VideoPlayerComponent } from './video-player/video-player.component';
+
+const routes: Routes = [
+  { path: '', component: VideoDashboardComponent },
+  { path: 'play/:id', component: VideoPlayerComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DashboardRoutingModule { }
